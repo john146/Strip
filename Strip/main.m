2d0315c4c8b995ae2543ce276f49938563c64b62
John Ahrens <john@john-ahrens.com>
2011-06-20 16:33:41 -0700

    Initial Commit

---
+++ Strip/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Strip
+//
+//  Created by John Ahrens on 6/20/11.
+//  Copyright 2011 John Ahrens, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
